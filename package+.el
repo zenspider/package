1e25d8788b413b4a9c4a30970164a306c80de5c4
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2013-11-11 18:02:27 +0100

    Update descriptions
    Update description of the package in README.md and in the lisp file
    itself.

--- package+.el
+++ package+.el
@@ -39,8 +39,8 @@
 ;; Provides extensions to `package.el` for Emacs 24 and later.
 
 ;; Declares a manifest of packages that should be installed on this
-;; system, installing any missing packages and removing any installed
-;; packages that are not in the manifest.
+;; system. Invoking package-sync will then install any missing packages 
+;; and remove any installed packages not in the manifest.
 ;;
 ;; This makes it easy to keep a list of packages under version control
 ;; and replicated across all your environments, without having to have
